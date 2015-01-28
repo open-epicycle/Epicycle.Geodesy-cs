df0150563fb1bc2a3f803dca00c8f39a5dc2a293
untrots <untrots@gmail.com>
2015-01-28 13:16:34 +0200

    Version: 0.1.2.0 => 0.1.3.0

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -2,28 +2,28 @@
 
 rmdir NuGetPackage /s /q
 mkdir NuGetPackage
-mkdir NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0
-mkdir NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib
+mkdir NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0
+mkdir NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib
 
-copy package.nuspec NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\Epicycle.Geodesy-cs.0.1.2.0.nuspec
-copy README.md NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\README.md
-copy LICENSE NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\LICENSE
+copy package.nuspec NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\Epicycle.Geodesy-cs.0.1.3.0.nuspec
+copy README.md NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\README.md
+copy LICENSE NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\LICENSE
 
-xcopy bin\net35\Release\Epicycle.Geodesy_cs.dll NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Geodesy_cs.pdb NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Geodesy_cs.xml NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net35\
-xcopy bin\net40\Release\Epicycle.Geodesy_cs.dll NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Geodesy_cs.pdb NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Geodesy_cs.xml NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net40\
-xcopy bin\net45\Release\Epicycle.Geodesy_cs.dll NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Geodesy_cs.pdb NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Geodesy_cs.xml NuGetPackage\Epicycle.Geodesy-cs.0.1.2.0\lib\net45\
+xcopy bin\net35\Release\Epicycle.Geodesy_cs.dll NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Geodesy_cs.pdb NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Geodesy_cs.xml NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net35\
+xcopy bin\net40\Release\Epicycle.Geodesy_cs.dll NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Geodesy_cs.pdb NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Geodesy_cs.xml NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net40\
+xcopy bin\net45\Release\Epicycle.Geodesy_cs.dll NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Geodesy_cs.pdb NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Geodesy_cs.xml NuGetPackage\Epicycle.Geodesy-cs.0.1.3.0\lib\net45\
 
 cd NuGetPackage
-nuget pack Epicycle.Geodesy-cs.0.1.2.0\Epicycle.Geodesy-cs.0.1.2.0.nuspec -Properties version=0.1.2.0
-7z a -tzip Epicycle.Geodesy-cs.0.1.2.0.zip Epicycle.Geodesy-cs.0.1.2.0 Epicycle.Geodesy-cs.0.1.2.0.nupkg
+nuget pack Epicycle.Geodesy-cs.0.1.3.0\Epicycle.Geodesy-cs.0.1.3.0.nuspec -Properties version=0.1.3.0
+7z a -tzip Epicycle.Geodesy-cs.0.1.3.0.zip Epicycle.Geodesy-cs.0.1.3.0 Epicycle.Geodesy-cs.0.1.3.0.nupkg
 
-echo nuget push Epicycle.Geodesy-cs.0.1.2.0.nupkg > push.cmd
+echo nuget push Epicycle.Geodesy-cs.0.1.3.0.nupkg > push.cmd
 echo pause >> push.cmd
 
 pause
